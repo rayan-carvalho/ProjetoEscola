ab84cb9b0e95ee91aaf01b1c28ec98b793fb7bd6
Rayan Carvalho <rayanbarroso@gmail.com>
2021-07-27 16:21:28 -0300

    first commit

---
+++ api/controllers/PessoaController.js
@@ -0,0 +1,19 @@
+const database = require('../models')
+
+class PessoaController{
+
+    static async pegaTodasAsPessoas(req, res){
+
+        try {
+            const pegaTodasAsPessoas = await database.Pessoas.findAll()
+            return res.status(200).json(pegaTodasAsPessoas)
+        } 
+        catch (error) {
+            return res.status(500).json(error.message)
+        }        
+
+    }
+
+}
+
+module.exports = PessoaController
