3f30efa18a49078ad9afa0ba48a7ae0ed0b19d02
Rayan Carvalho <rayanbarroso@gmail.com>
2021-07-28 10:20:15 -0300

    CRUD Completo

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -70,6 +70,24 @@ class PessoaController{
 
     }
 
+    static async apagarPessoa(req, res){
+
+        const { id } = req.params       
+
+        try {
+            await database.Pessoas.destroy({
+             where: {
+                    id:Number(id)
+                }
+            })          
+            return res.status(200).json({mensagem: `id ${id} deletado`})
+        } 
+        catch (error) {
+            return res.status(500).json(error.message)
+        } 
+
+    }
+
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,5 +6,7 @@ const router =  Router()
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 router.post('/pessoas', PessoaController.criarPessoa)
+router.put('/pessoas/:id', PessoaController.atualizarPessoa)
+router.delete('/pessoas/:id', PessoaController.apagarPessoa)
 
 module.exports = router
